aeb83e37cf1d1bdac3c60c2485b3f3fcfbc5498c
KrishnaP <kpat132@gmail.com>
2018-01-26 15:24:01 -1000

    GET WORKS BUT NOT 404

--- server.js
+++ server.js
@@ -1,40 +1,46 @@
 const http = require('http');
 const fs = require('fs');
+const querystring = require('querystring');
 
 const PORT = process.env.PORT || 8080;
 const server = http.createServer((request, response) => {
-
-  //Client requests the method, http version and url
-  // console.log(request.method);
-  // console.log(request.httpVersion);
-  //console.log(request.url);
 
   let url = request.url;
   let method = request.method;
 
 
+  // console.log(request.method);
+  // console.log(request.httpVersion);
+  // console.log(request.url);
+
+
+
   switch (method) {
 
     case 'GET':
+
       if (url === `/`) {
         url = `/index.html`;
       }
-      if(url === `/css/styles.css`){
-        url === `/css/styles.css`;
-        getCssRequest(url, request, response );
-       break;
+
+      if (url === `/css/styles.css`) {
+        getCssRequest(url, request, response);
+        break;
       }
 
       getRequests(url, request, response);
       break;
 
-    case 'POST':
-
-      response.end();
-      break;
-
     default:
-      response.end();
+
+      fs.readFile(`/public/404.html`, (err, data) => {
+        console.log("data: " + data);
+        response.writeHead(404, 'Not Found');
+        response.write(data);
+        return response.end();
+      });
+
+      //response.end();
       break;
   }
 
@@ -46,15 +52,15 @@ server.listen(PORT, () => {
 //Error and success headers
 function errorResponse(response) {
   response.writeHead(404, 'Not Found');
+
 }
 function successHeader(response) {
   response.setHeader('Content-Type', 'text/html');
   response.writeHead(200, 'OK');
 }
 
-
 // GETREQUEST
-function getRequests(url, req, res ) {
+function getRequests(url, req, res) {
   fs.readFile(`public${url}`, { encoding: 'utf8' }, (err, data) => {
     //console.log(`public${url}`);
 
@@ -68,13 +74,13 @@ function getRequests(url, req, res ) {
     return res.end();
   });
 }
-
-function getCssRequest(url, req, res ) {
+// GETREQUEST FOR CSS
+function getCssRequest(url, req, res) {
 
   fs.readFile(`public${url}`, { encoding: 'utf8' }, (err, data) => {
     console.log(`public${url}`);
 
-    if (err) {
+    if (!(`public${url}`)) {
       errorResponse(res);
       return res.end();
     }

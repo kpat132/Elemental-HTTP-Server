3945ffddb74c6a86f4dc9059f8c0a7973d3f1fb8
KrishnaP <kpat132@gmail.com>
2018-01-29 11:48:56 -1000

    can POST a new file; but the 404 doesnt work and needs to show up on the indexhtml

--- server.js
+++ server.js
@@ -12,47 +12,83 @@ const server = http.createServer((request, response) => {
   // console.log(request.method);
   // console.log(request.httpVersion);
   // console.log(request.url);
+  get(url)
 
 
+  function get(url) {
 
-  switch (method) {
+    switch (url) {
 
-    case 'GET':
-
-      if (url === `/`) {
+      case `/`:
         url = `/index.html`;
-      }
+        getRequests(url, request, response);
+        break
 
-      if (url === `/css/styles.css`) {
+      // case `/index.html`:
+
+      //   getRequests(url, request, response);
+      //   break;
+
+      // case `/hydrogen.html`:
+      //   getRequests(url, request, response);
+      //   break;
+
+      // case `/helium.html`:
+      //   getRequests(url, request, response);
+      //   break;
+
+      case `/css/styles.css`:
         getCssRequest(url, request, response);
         break;
-      }
-
+      case url:
       getRequests(url, request, response);
       break;
 
-    default:
+      default:
 
-      fs.readFile(`/public/404.html`, (err, data) => {
-        console.log("data: " + data);
-        response.writeHead(404, 'Not Found');
-        response.write(data);
-        return response.end();
-      });
+        fs.readFile(`./public/404.html`, (err, data) => {
+          response.writeHead(404, 'Not Found');
+          response.write(data);
+          return response.end();
+        });
 
-      //response.end();
-      break;
+        //response.end();
+        break;
+    }
   }
 
+  if(method === "POST"){
+
+    request.on('data',(chunk)=>{
+      let chunkStr = chunk.toString();
+      let post = querystring.parse(chunkStr);
+      //console.log(post);
+
+      makeFile(post);
+      //console.log(url)
+      get(url);
+    }); 
+  }
 });
+
 server.listen(PORT, () => {
   console.log(`Server is listening: ${PORT}`);
 });
 
+function makeFile(post){
+  let elementBody = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>The Elements - ${post.elementName}</title><link rel="stylesheet" href="/css/styles.css"></head><body><h1>${post.elementName}</h1><h2>H</h2><h3>Atomic number ${post.elementAtomicNumber}</h3><p>${post.elementName} is a chemical element with symbol ${post.elementSymbol} and atomic number ${post.elementAtomicNumber}. It is ${post.elementDescription}</p><p><a href="/">back</a></p></body></html>`
+  fs.writeFile(`public/${post.elementName}.html`,elementBody ,(err) =>{
+    if(err){
+      throw err;
+    }
+    console.log("FILE POSTED :D");
+  });
+
+  }
+
 //Error and success headers
 function errorResponse(response) {
   response.writeHead(404, 'Not Found');
-
 }
 function successHeader(response) {
   response.setHeader('Content-Type', 'text/html');
